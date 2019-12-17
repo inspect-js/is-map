a0a0d4a089ce73999f7f54479e0638c479305bd1
Jordan Harband <ljharb@gmail.com>
2019-12-17 10:59:44 -0800

    [Refactor] avoid top-level return, because babel and webpack are broken
    Closes #5. Closes #4. Closes #3.
    
    Closes https://github.com/inspect-js/node-deep-equal/issues/78.
    Closes https://github.com/es-shims/Promise.allSettled/issues/7.
    Closes https://github.com/airbnb/js-shims/issues/12.
    
    Relates to https://github.com/inspect-js/is-set/pull/4, https://github.com/inspect-js/is-set/pull/5
    
    Addresses https://github.com/storybookjs/storybook/issues/9154.

--- index.js
+++ index.js
@@ -3,27 +3,27 @@
 var $Map = typeof Map === 'function' && Map.prototype ? Map : null;
 var $Set = typeof Set === 'function' && Set.prototype ? Set : null;
 
+var exported;
+
 if (!$Map) {
 	// eslint-disable-next-line no-unused-vars
-	module.exports = function isMap(x) {
+	exported = function isMap(x) {
 		// `Map` is not present in this environment.
 		return false;
 	};
-	return;
 }
 
 var $mapHas = $Map ? Map.prototype.has : null;
 var $setHas = $Set ? Set.prototype.has : null;
-if (!$mapHas) {
+if (!exported && !$mapHas) {
 	// eslint-disable-next-line no-unused-vars
-	module.exports = function isMap(x) {
+	exported = function isMap(x) {
 		// `Map` does not have a `has` method
 		return false;
 	};
-	return;
 }
 
-module.exports = function isMap(x) {
+module.exports = exported || function isMap(x) {
 	if (!x || typeof x !== 'object') {
 		return false;
 	}
